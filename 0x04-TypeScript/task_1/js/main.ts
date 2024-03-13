12d8af71b26c192bcd71c6a982aebf5b4373af39
Gabriel Muthenya <gmthex@gmail.com>
2024-03-13 16:40:33 +0000

    4. Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -33,3 +33,70 @@ export interface printTeacherFunction {
 export function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
+
+/**
+ * Represents a constructor function for creating instances of IStudentClass.
+ */
+export interface IStudentClassConstructor {
+  /**
+   * Constructs a new instance of IStudentClass.
+   * @param firstName - The first name of the student.
+   * @param lastName - The last name of the student.
+   * @returns An instance of IStudentClass.
+   */
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+/**
+ * Specifies the methods that a student class should implement.
+ */
+export interface IStudentClass {
+  /**
+   * Indicates that the student is currently working on homework.
+   * @returns A string indicating the student's current activity.
+   */
+  workOnHomework(): string;
+
+  /**
+   * Retrieves the display name of the student.
+   * @returns A string representing the display name of the student.
+   */
+  displayName(): string;
+}
+
+/**
+ * Represents a concrete implementation of a student class.
+ */
+export class StudentClass implements IStudentClass {
+  /** The first name of the student. */
+  private _firstName!: string;
+
+  /** The last name of the student. */
+  private _lastName!: string;
+
+  /**
+   * Constructs a new instance of StudentClass.
+   * @param firstName - The first name of the student.
+   * @param lastName - The last name of the student.
+   */
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  /**
+   * Indicates that the student is currently working on homework.
+   * @returns A string indicating the student's current activity.
+   */
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  /**
+   * Retrieves the display name of the student.
+   * @returns A string representing the display name of the student.
+   */
+  displayName() {
+    return this._firstName;
+  }
+}
